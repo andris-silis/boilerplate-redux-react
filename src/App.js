c2004f8a80a6c6301144b9a70ab7007972f2a2c7
Andris Silis <andris.silis@gmail.com>
2016-03-10 16:00:45 +0200

    Redux devtools Chrome extension support

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Map } from "immutable";
 import React, { Component } from "react";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 
 import centralReducer from "./central-reducer";
 import Layout from "./ui/layout/Layout";
 
 
-const store = createStore(centralReducer, Map());
+const createStoreWithMiddleware = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
+const store = createStoreWithMiddleware(centralReducer, Map());
 
 if (process.env.NODE_ENV === "development") {
   window.store = store;
+  window.dispatch = store.dispatch;
 }
 
 export class App extends Component {
