384566df1b5862e8e299bf1efba8ce5a58c399e6
Andris Silis <andris.silis@gmail.com>
2016-03-14 11:15:36 +0200

    Redux-Thunk

---
+++ src/modules/thunk.js
@@ -0,0 +1,14 @@
+export function thunkAction() {
+  return async(dispatch, getState) => {
+    // fetching
+    // dispatch();
+
+    // const response = await window.fetch("/");
+    // await response.json();
+
+    // fetched
+    // dispatch();
+    // save
+    // dispatch();
+  }
+}
